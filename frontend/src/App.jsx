3e5a86d14f06cad3176663163f39f73452c398bd
vipin_760 <vipinm500@gmail.com>
2024-05-31 02:00:40 +0530

    navbar 2 completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,17 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/partials/Header/Header";
 import "responsive-navbar-react/dist/index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header />
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Header />}/>
+    </Routes>
+    </BrowserRouter>
+      
     </>
   );
 }
--- frontend/src/components/partials/Header/Header.jsx
+++ frontend/src/components/partials/Header/Header.jsx
@@ -1,42 +1,33 @@
 import React, { useState } from "react";
-import { Navbar } from "responsive-navbar-react";
+import "./Header.css"
+import { ReactNavbar } from "overlay-navbar";
 const Header = () => {
-  const props = {
-    items: [
-      {
-        text: 'Home',
-        link: '/'
-      },
-      {
-        text: 'Doc',
-        link: '#docs'
-      },
-      {
-        text: 'Custom',
-        link: '#custom-bar'
-      },
-      {
-        text: 'Contact',
-        link: '#contact'
-      }
-    ],
-    logo: {
-      text: 'Responsive Navbar React'
-    },
-    style: {
-      barStyles: {
-        background: '#444'
-      },
-      sidebarStyles: {
-        background: '#222',
-        buttonColor: 'white'
-      }
-    }
-  }
   return (
-   <>
-   <Navbar {...props} />
-   </>
+    <>
+    <div className="nav">
+      <div className="logo">
+        logon
+      </div>
+  <input type="checkbox" id="nav-check" />
+  <div className="nav-header">
+  </div>
+  <div className="nav-btn">
+    <label htmlFor="nav-check">
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
+  </div>
+  
+  <div className="nav-links">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+    <a href="#">Link 4</a>
+    <a href="#">Link 5</a>
+  </div>
+</div>
+    </>
   );
 };
 
